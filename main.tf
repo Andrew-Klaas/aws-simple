b659850cb6ab917b653cd32b5addd78ea7d34771
Andrew Klaas <aklaas@hashicorp.com>
2017-11-03 10:33:40 -0500

    adding sg

--- main.tf
+++ main.tf
@@ -12,3 +12,33 @@ resource "aws_instance" "test" {
   }
   //*/
 }
+
+resource "aws_security_group" "private_network_access" {
+  name        = "private_network_access"
+  description = "security group for private network access"
+  vpc_id      = "${data.aws_vpc.default.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.16.0.0/16"]
+    #cidr_blocks = ["0.0.0.0./0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
