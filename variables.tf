748245b9723ca0d1603110e35805eb765af19313
Andrew Klaas <aklaas@hashicorp.com>
2019-09-22 17:12:04 -0500

    adding variables

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+    default = "ami-c998b6b2"
+}
+variable "instance_count" {
+    default = "1"
+} 
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "region" {
+    default = "us-east-1"
+}
